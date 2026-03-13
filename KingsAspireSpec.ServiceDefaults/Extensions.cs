c9f04997bb500a734f95d8c2139adf45329eee94
Jay Thumma Yh <jthumma@yahoo.com>
2026-03-13 14:48:17 -0400

    er

--- KingsAspireSpec.ServiceDefaults/Extensions.cs
+++ KingsAspireSpec.ServiceDefaults/Extensions.cs
@@ -10,6 +10,8 @@ using OpenTelemetry.Trace;
 
 namespace KingsAspireSpec.ServiceDefaults;
 
+
+
 // Adds common Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
 // This project should be referenced by each service project in your solution.
 // To learn more about using this project, see https://aka.ms/dotnet/aspire/service-defaults
